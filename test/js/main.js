b317e4f400019d61f9545f2d5eda221b13cbe916
nikolaytuz <nikolaytuz56@gmail.com>
2020-03-30 17:18:06 +0400

    последнииее

--- test/js/main.js
+++ test/js/main.js
@@ -2,7 +2,24 @@
 
 var app = new Vue({
   el: '#app',
+  created: function() {
+    this.addtable()
+  },
+  methods:{
+    "addtable": function() {
+      for (var i = 0; i < this.y; i++) {
+        let a = []
+        for (var j = 0; j < this.y; j++) {
+
+        }
+      }
+      this.table =
+    },
+  },
   data: {
+    x: 16,
+    y: 16,
+    table: [],
     message: 'Привет, Vue!'
   }
 })
