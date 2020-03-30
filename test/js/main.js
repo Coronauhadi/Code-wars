04bd2b185a6ac6018ead0446e48867516d74b18c
Matvey <tod13yung@yandex.ru>
2020-03-30 21:22:46 +0400

    Rotation method for pers

--- test/js/main.js
+++ test/js/main.js
@@ -4,6 +4,7 @@ var app = new Vue({
   el: '#app',
   created: function() {
     this.addtables()
+	this.setcoor("x"+this.pers.cor.x+"y"+this.pers.cor.y)
   },
   mounted: function() {
     this.setcoor("x1y1")
@@ -35,15 +36,49 @@ var app = new Vue({
       this.pers.pos.left = cor.left+'px'
     },
     "addshg": function() {
-      this.pers.cor.y++
+      if(this.pers.direction == 0){
+		  this.pers.cor.y++
+	  }
+	  else if(this.pers.direction == 1){
+		  this.pers.cor.x++
+	  }
+	  else if(this.pers.direction == 2){
+		  this.pers.cor.y--
+	  }
+	  else if(this.pers.direction == 3){
+		  this.pers.cor.x--
+	  }
+	  else{
+		  return;
+	  }
+	  
       this.setcoor("x"+this.pers.cor.x+"y"+this.pers.cor.y)
     },
+	"rotate": function(dir){
+		if(dir == 'left' || dir == 'l'){
+			this.pers.direction = Math.abs((this.pers.direction -1) % 4)
+		}
+		else if(dir == 'right' || dir == 'r'){
+			this.pers.direction = Math.abs((this.pers.direction +1) % 4)
+		}
+		else{
+			return;
+		}
+		this.pers.pos.transform = 'rotate(' + String(Math.round(this.pers.direction * 90)) + 'deg)'
+	},
   },
   data: {
     windowWidth: window.innerWidth,
     pers: {
-      cor:{'x': 1, 'y': 1},
-      pos: {"top": "1px", "left":"1px",}
+      cor:{'x': 5, 'y': 5},
+      pos: {"top": "1px", "left":"1px", "transform": "rotate(0 deg)",},
+	  /*
+	  direction = 0 -> look right
+	  direction = 1 -> look bottom
+	  direction = 2 -> look left
+	  direction = 3 -> look look top
+	  */
+	  direction: 0,
     },
     x: 16,
     y: 16,
@@ -62,15 +97,3 @@ var app = new Vue({
 
 })
 
-let tcells = document.querySelectorAll("table#fields tr > th")
-
-console.log(getCoords(tcells[tcells.length-1]))
-
-function getCoords(elem) {
-      var box = elem.getBoundingClientRect();
-
-      return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset
-	  }
-}
